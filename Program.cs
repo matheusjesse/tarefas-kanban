2ebda7d7e4c4af3e65dbf218cd10e332ce93b266
matheus jesse <matheus_jesse@hotmail.com>
2024-02-23 14:52:05 -0300

    feat: configurando tailwind no projeto

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 using tarefas_kanban.Components;
+using Blazorise;
+using Blazorise.Tailwind;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
+
+
 
 var app = builder.Build();
 
